9c014e447d4d8b90a0074a5fc8c6ec722e300bfc
zhenyuwei99 <zhenyuwei99@gmail.com>
2020-04-29 17:16:14 +0800

    v-0.0.2

--- src/.ipynb_checkpoints/lmp_data-checkpoint.jl
+++ src/.ipynb_checkpoints/lmp_data-checkpoint.jl
@@ -15,10 +15,14 @@ include("useful_funcs.jl")
 export dist, str2float, diag, copy_array
 
 include("read_dump.jl")
-export read_dump, read_dump_prim, split_dump!, momentum!, mass!
+export read_dump, read_dump_prim, split_dump!, momentum!, mass!, time_step!
 
 include("box.jl")
 export genr_box_diag, genr_box_inv, PBC!
+
+include("diffusion.jl")
+export MSD!, diffusion
+
 
 end # module
 
